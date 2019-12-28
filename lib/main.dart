701cd5d588fa8a335e94414e3e63d632a31a0e56
Khoa Vo <khoa.sdk@gmail.com>
2019-12-28 15:14:27 +0700

    Initilze Pizza App

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:app_widgets/screens/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: "Simple Layouts",
+      home: HomeScreen()
+    ),
+  );
+}
