2a714d2c989fd869692852c9c7829c10581623a5
Khoa Vo <khoa.sdk@gmail.com>
2019-12-28 16:28:38 +0700

    Add Order Pizza Function

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -6,16 +6,61 @@ class HomeScreen extends StatelessWidget {
     return Container(
       alignment: Alignment.center,
       color: Colors.deepOrangeAccent,
-      child: Text(
-        'Pizza',
-        textDirection: TextDirection.ltr,
-        style: TextStyle(
-          fontSize: 80,
-          decoration: TextDecoration.none,
-          fontFamily: 'Oxygen',
-          fontWeight: FontWeight.normal,
-        ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            'Pizza',
+            textDirection: TextDirection.ltr,
+            style: TextStyle(
+              fontSize: 64,
+              decoration: TextDecoration.none,
+              color: Colors.deepPurple,
+              fontFamily: 'Oxygen',
+              fontWeight: FontWeight.normal,
+            ),
+          ),
+          PizzaImageWidget(),
+          OrderButtonWidget(),
+        ],
       ),
+    );
+  }
+}
+
+class PizzaImageWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    AssetImage pizzaImage = AssetImage('images/pizza.png');
+    Image image = Image(image: pizzaImage);
+    return Container(child: image);
+  }
+}
+
+class OrderButtonWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: RaisedButton(
+        child: Text('Order your Pizza!'),
+        color: Colors.lightGreen,
+        elevation: 5,
+        onPressed: () {
+          order(context);
+        },
+      ),
+    );
+  }
+
+  void order(BuildContext context) {
+    AlertDialog alert = AlertDialog(
+      title: Text('Order Completed'),
+      content: Text('Thanks for your order!'),
+    );
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) => alert
     );
   }
 }
